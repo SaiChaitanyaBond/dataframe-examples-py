4dd15540f4445b059212a6e2868f85e0f4f3050c
SaiChaitanyaBond <saichaitanyabondada@gmail.com>
2022-07-16 09:40:42 -0500

    test

--- dataframe/curation/dsl/udf_demo.py
+++ dataframe/curation/dsl/udf_demo.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, lit
+from pyspark.sql.functions import *
 from pyspark.sql.types import StringType
 
 if __name__ == '__main__':
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
-    def initcap(line: str):
+    def initcappp(line: str):
         lst = line.split(" ")
         return ' '.join(list(map(str.capitalize, lst)))
 
@@ -23,10 +23,10 @@ if __name__ == '__main__':
 
     # Register: Method1
     initcap_udf1 = spark.udf\
-        .register("initcap", initcap, StringType())
+        .register("initcap", initcappp, StringType())
 
     # Register: Method2
-    initcap_udf2 = udf(initcap, StringType())
+    initcap_udf2 = udf(initcappp, StringType())
 
     # Register: Method3
     initcap_udf3 = spark.udf\
@@ -40,7 +40,8 @@ if __name__ == '__main__':
                     initcap_udf1("text").alias("text1"),
                     initcap_udf2("text").alias("text2"),
                     initcap_udf3("text").alias("text3"),
-                    initcap_udf4("text", lit(" ")).alias("text4")
+                    initcap_udf4("text", lit(" ")).alias("text4"),
+                    initcap("text").alias("text5")
                     )\
         .show(5, False)
 
